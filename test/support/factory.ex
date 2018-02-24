bb6777763ce874d8622e9eea16d24ff786fab98a
Ben Smith <ben@10consulting.com>
2018-02-24 13:34:53 +0000

    Remove schedule projection when cancelled

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -11,7 +11,7 @@ defmodule Commanded.Scheduler.Factory do
     schedule_uuid = UUID.uuid4()
     schedule_name = "timeout_reservation"
     ticket_uuid = Map.get(context, :ticket_uuid, UUID.uuid4())
-    due_at = NaiveDateTime.utc_now()
+    due_at = NaiveDateTime.utc_now() |> NaiveDateTime.add(60, :second)
 
     command = %TimeoutReservation{
       ticket_uuid: ticket_uuid
